90211f311ab84c95086303a532507d9f8d6bc622
Eetu Tuomala <laardee@outlook.com>
2018-02-14 07:50:51 +0200

    initial commit

---
+++ handler.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports.helloWorld = (event, context, callback) => {
+  console.log(JSON.stringify(event, null, 2));
+  const response = event.Records[0].cf.response;
+  const headers = response.headers;
+
+  headers['x-time-now'] = [
+      { key: 'x-time-now', value: Date.now().toString() },
+  ];
+
+  return callback(null, response);
+};
