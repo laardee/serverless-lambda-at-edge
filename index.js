0a562daa1f2cc07ff85b0142cc390631491dd44c
Eetu Tuomala <eetu.tuomala@maas.global>
2019-09-04 17:46:07 +0300

    use viewer response

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 module.exports.handler = async event => {
   console.log(JSON.stringify(event, null, 2));
-  const response = event.Records[0].cf.request;
+  const response = event.Records[0].cf.response;
   const headers = response.headers;
 
   headers['x-time-now'] = [
